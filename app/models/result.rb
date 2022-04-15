2c936d53b04ee65b170a0a9a78f12ebaafb7451b
iwamotokazuya <kazuya10211026@gmail.com>
2022-04-15 12:07:08 +0900

    Fix: score修正

--- app/models/result.rb
+++ app/models/result.rb
@@ -11,7 +11,7 @@ class Result < ApplicationRecord
   validates :energy, numericality: { only_integer: true }, presence: true
 
   def empath(formdata)
-    conn = Faraday::Connection.new('url': 'https://api.webempath.net/v2/analyzeWav') do |f|
+    conn = Faraday::Connection.new('url': ENV['API_URL']) do |f|
       f.request :multipart
       f.request :url_encoded
       f.response :logger
@@ -20,7 +20,7 @@ class Result < ApplicationRecord
 
     response = conn.post do |req|
       req.body = {
-        apikey: 'LA2gB0HqQmogATe9uNpJa4iUoChXc02hJyxJGF07K-4',
+        apikey: ENV['API_KEY'],
         wav: Faraday::Multipart::FilePart.new(formdata[:record_voice], 'audio/wav')
       }
     end
@@ -34,7 +34,7 @@ class Result < ApplicationRecord
       self.joy = hash['joy']
       self.sorrow = hash['sorrow']
       self.energy = hash['energy']
-      self.score = hash['anger'] * 2
+      self.score = (25 + (0.5 * (anger) + 0.25 * (sorrow + energy))).round
     record_voice.attach(formdata[:record_voice])
   end
 end
