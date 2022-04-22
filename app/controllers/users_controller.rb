631d0f828012017f3d2b3dc6b840c551d94f10ef
iwamotokazuya <kazuya10211026@gmail.com>
2022-04-22 15:12:13 +0900

    Add: ログイン、新規登録画面作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+    @result = Result.find(params[:result_id]) if params[:result_id]
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to login_path
+      flash[:notice] = 'ユーザーの作成に成功しました'
+    else
+      flash.now[:alert] = 'ユーザーの作成に失敗しました'
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
