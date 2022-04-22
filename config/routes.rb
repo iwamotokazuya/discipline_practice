e5b483e8f9815522753d084882ae838bf8623085
iwamotokazuya <kazuya10211026@gmail.com>
2022-04-22 15:12:44 +0900

    Add: ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   root 'records#new'
   
   resources :results, only: %i[show create]
+
+  resources :users
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
