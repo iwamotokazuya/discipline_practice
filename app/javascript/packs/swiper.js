37f961e569b4d78ad343188ed4a4ba97771bfe22
iwamotokazuya <kazuya10211026@gmail.com>
2022-05-25 16:43:56 +0900

    Add: swiper導入

---
+++ app/javascript/packs/swiper.js
@@ -0,0 +1,19 @@
+const mySwiper = new Swiper('.mv01 .swiper', {
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
+  loop: true,
+  loopAdditionalSlides: 1,
+  speed: 2000,
+  autoplay: {
+    delay: 7000,
+    disableOnInteraction: false,
+    waitForTransition: false,
+  },
+  followFinger: false,
+  pagination: {
+    el: '.mv01 .swiper-pagination',
+    clickable: true,
+  },
+});
