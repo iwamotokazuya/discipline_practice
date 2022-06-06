17e1d12f7ff9917a61bfa6edd712872884ed17bc
iwamotokazuya <kazuya10211026@gmail.com>
2022-06-06 11:55:55 +0900

    Fix: ゲストユーザー指定

--- db/fixtures/user.rb
+++ db/fixtures/user.rb
@@ -1,4 +1,4 @@
-if User.exists?
+if User.exists?(1)
   User.update(id: 1, name: 'guest', email: 'guest@example.com', password: 'password', password_confirmation: 'password')
 else
   User.create!(id: 1, name: 'guest', email: 'guest@example.com', password: 'password', password_confirmation: 'password')
