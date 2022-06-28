89acf7dc8741fe7463876e53e6360ab6486abb56
iwamotokazuya <kazuya10211026@gmail.com>
2022-06-28 18:43:27 +0900

    Add: データベースにカラムが一意であること追記

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_27_093618) do
+ActiveRecord::Schema.define(version: 2022_06_28_093955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2022_06_27_093618) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["result_id"], name: "index_likes_on_result_id"
+    t.index ["user_id", "result_id"], name: "index_likes_on_user_id_and_result_id", unique: true
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
