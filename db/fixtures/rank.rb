f67700d4435d99e5bd42d1cdeffa42e3bc3ad166
iwamotokazuya <kazuya10211026@gmail.com>
2022-07-04 16:26:33 +0900

    Add: rankデータ挿入・ゲストデータ更新できるよう変更

---
+++ db/fixtures/rank.rb
@@ -0,0 +1,5 @@
+Rank.seed(:id,
+{ :id => 1, :level => '初級', :subtitle => 'しつけには冷静さも必要だ' },
+{ :id => 2, :level => '中級', :subtitle => '冷静かつ勢いも大切に' },
+{ :id => 3, :level => '上級', :subtitle => '純粋な怒りのみ測定' }
+)
--- db/fixtures/user.rb
+++ db/fixtures/user.rb
@@ -1,1 +1,7 @@
-User.create!(id: 1, name: 'guest', email: 'guest@example.com', password: 'password', password_confirmation: 'password')
+if User.exists?(1)
+  User.update(id: 1, name: 'guest', email: 'guest@example.com',
+    password: 'password', password_confirmation: 'password')
+else
+  User.create!(id: 1, name: 'guest', email: 'guest@example.com',
+    password: 'password', password_confirmation: 'password')
+end
