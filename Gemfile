ad505e111cf9649ded70a8bdac716221e0586eb6
iwamotokazuya <kazuya10211026@gmail.com>
2022-07-17 13:00:42 +0900

    Add: gem meta-tags設定

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'image_processing', '~> 1.2'
 gem "simple_calendar", "~> 2.4"
 gem 'rails-i18n'
 gem 'config'
+gem 'meta-tags'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'しつけメーター',
+      title: 'ほどよい怒りを身につけませんか？',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'ペットが可愛くて仕方なく、あまり怒れない人に怒る基準を明確にして, しつけの練習をするサービスです。',
+      keywords: 'ペット,しつけ',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url('dog-icon.jpg') },
+        { href: image_url('dog-icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('dog-icon.jpg'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary',
+      }
+    }
+  end
 end
